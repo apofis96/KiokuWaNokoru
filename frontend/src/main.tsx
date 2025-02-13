059cc9e68ed0f914d00dadf44ce3dc099bfb080a
Anton Komisarov <32576664+apofis96@users.noreply.github.com>
2025-02-13 23:05:31 +0200

    Add project starter

---
+++ frontend/src/main.tsx
@@ -0,0 +1,19 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { ChakraProvider, defaultSystem} from '@chakra-ui/react'
+import { ThemeProvider } from 'next-themes'
+import './index.css'
+import App from './App.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <ChakraProvider value={defaultSystem}>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme='dark'
+      >
+        <App />
+      </ThemeProvider>
+    </ChakraProvider>
+  </StrictMode>,
+)
